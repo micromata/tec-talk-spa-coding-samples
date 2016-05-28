07c4f8efa581c4cc60c14fc1ada4d30ff723f956
René Viering <r.viering@micromata.de>
2016-05-28 19:04:33 +0200

    Finish one-way-databinding with initial property trigger

--- src/app/mvvm/_pattern/applyDatabinding.js
+++ src/app/mvvm/_pattern/applyDatabinding.js
@@ -1,22 +1,30 @@
+import $ from 'jquery';
+
 const applyDataBinding = viewModel => {
 
 	const applyBindingFromModelToDom = () => {
+
+		const onViewModelPropertyChanged = propertyName => {
+			// Get the newest value of the viewmodel property
+			const newValue = viewModel[propertyName].get();
+
+			// Get the DOM Element with the correct propertyName
+			const selector = `[data-bind='${propertyName}']`;
+
+			// set the new value
+			$(selector).html(newValue);
+			$(selector).val(newValue);
+		};
 
 		Object.keys(viewModel).forEach(propertyName => {
 			if (typeof (viewModel[propertyName].subscribe) !== 'undefined') {
 				viewModel[propertyName].subscribe(() => {
 
-					// Get the newest value of the viewmodel property
-					const newValue = viewModel[propertyName].get();
-
-					// Get the DOM Element with the correct propertyName
-					const selector = `[data-bind='${propertyName}']`;
-
-					// set the new value
-					$(selector).html(newValue);
-					$(selector).val(newValue);
+					onViewModelPropertyChanged(propertyName);
 
 				});
+
+				onViewModelPropertyChanged(propertyName);
 			}
 		});
 
