839ef9e62d9d0d24fa8e67cba154ad7518a05b5f
René Viering <r.viering@micromata.de>
2016-05-28 17:57:40 +0200

    Use hashchange event

--- src/app/core/router.js
+++ src/app/core/router.js
@@ -1,12 +1,28 @@
 const createRouter = () => {
+	const routes = {};
 
-	const addRoute = () => {
-
+	const addRoute = (routeUrl, routeHandler) => {
+		routes[routeUrl] = routeHandler;
 	};
 
 	const navigateToHashUrl = () => {
 
 	};
+
+	const handleRouting = () => {
+		// i.e. http://www.awesome-site.io/#home => home
+		const url = location.hash.slice(1);
+		const routeHandler = routes[url];
+
+		if (routeHandler) {
+			routeHandler();
+		}
+	};
+
+	if (window) {
+		window.addEventListener('hashchange', handleRouting);
+		window.addEventListener('load', handleRouting);
+	}
 
 	return {addRoute, navigateToHashUrl};
 };
